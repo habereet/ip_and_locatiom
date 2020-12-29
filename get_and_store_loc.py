8af2c799963eab57532a555e0448ce73616b03dd
habereet <habereet@gmail.com>
2020-12-28 23:48:00 -0500

    initial commit from console

---
+++ get_and_store_loc.py
@@ -0,0 +1,10 @@
+from location_info import location_info
+
+
+def main():
+    li = location_info()
+    print(li.local_ip)
+
+
+if __name__ == "__main__":
+    main()
---
+++ location_info.py
@@ -0,0 +1,8 @@
+from requests import get
+
+class location_info():
+    def __init__(self):
+        self.local_ip = self.set_local_ip()
+
+    def set_local_ip(self):
+        return get('https://api.ipify.org').text

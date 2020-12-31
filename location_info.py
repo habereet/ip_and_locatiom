c9b4b5810a6fa0cba3e0ebd8cedbbbd5cd581ba2
habereet <habereet@gmail.com>
2020-12-30 21:48:23 -0500

    started get location

--- location_info.py
+++ location_info.py
@@ -1,9 +1,18 @@
 from requests import get
+import subprocess
 
 
 class location_info():
     def __init__(self):
         self.local_ip = self.set_local_ip()
+        self.location = self.set_location()
 
     def set_local_ip(self):
         return get('https://api.ipify.org').text
+
+    def set_location(self):
+        status, output = subprocess.getstatusoutput("termux-wifi-connectioninfo")
+        if status == 0:
+          return 1
+        else:
+          return 0

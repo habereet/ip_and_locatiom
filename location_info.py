3a313bb482fa3805318d724d29f2517ed3f33500
habereet <habereet@gmail.com>
2021-01-02 12:19:44 -0500

    added get wifi status

--- location_info.py
+++ location_info.py
@@ -1,18 +1,31 @@
 from requests import get
 import subprocess
+import json
 
 
 class location_info():
     def __init__(self):
         self.local_ip = self.set_local_ip()
-        self.location = self.set_location()
+        #self.location = self.set_location()
+        print(self.set_location())
+        print(self.set_wifi())
+        #self.wifi = self.set_wifi()
 
     def set_local_ip(self):
         return get('https://api.ipify.org').text
 
     def set_location(self):
         status, output = subprocess.getstatusoutput(
-            "termux-wifi-connectioninfo"
+            "termux-location"
+        )
+        if status == 0:
+            return 1
+        else:
+            return 0
+    
+    def set_wifi(self):
+        status, output = subprocess.getstatusoutput(
+          "termux-wifi-connectioninfo"
         )
         if status == 0:
             return 1
